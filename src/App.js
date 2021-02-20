531e55f1a054c95a7d9d4dc8ad73cc10613d43d9
Jasmine Dyson <J.dyson89@yahoo.com>
2021-02-20 09:38:29 -0500

    react-notes added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+let studentData = [
+  {
+    id: 1,
+    name: "Eddy",
+    color: "chartreuse",
+    school: "Nebula Academy",
+  },
+  {
+    id: 2,
+    name: "Chelsea",
+    color: "purple",
+    school: "Nebula Academy",
+  },
+  {
+    id: 3,
+    name: "Tauseef",
+    color: "orange",
+    school: "Nebula Academy",
+  },
+  {
+    id: 4,
+    name: "Maurice",
+    color: "red",
+    school: "Nebula Academy",
+  },
+  {
+    id: 5,
+    name: "D'aja",
+    color: "blue",
+    school: "Nebula Academy",
+  },
+  {
+    id: 6,
+    name: "Jasmine",
+    color: "pink",
+    school: "Nebula Academy",
+  },
+  {
+    id: 7,
+    name: "Anitria",
+    color: "blue",
+    school: "Nebula Academy",
+  },
+];
+
+
+//injecting a function
+// function App(){
+//   return (
+//     <div>
+//       <StudentList />
+//       <StudentList />
+//       <StudentList />
+//       <StudentList />
+//     </div>
+//   );
+// }
+
+function App(){
+  return studentData.map((student) => (
+    <div className='studentCard'>
+      <h1>{student.name} is a {student.school} student</h1>
     </div>
-  );
+  )); 
 }
 
 export default App;
